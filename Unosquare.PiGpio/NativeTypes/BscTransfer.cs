9c6cde59346fce6ea89fcc561c7a78b7be3489e7
Mario Di Vece <mario@unosquare.com>
2018-03-29 21:39:08 -0600

    more fixes.

--- Unosquare.PiGpio/NativeTypes/BscTransfer.cs
+++ Unosquare.PiGpio/NativeTypes/BscTransfer.cs
@@ -8,31 +8,41 @@
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public class BscTransfer
     {
+        private uint m_Control;
+
+        private int m_ReceiveCount;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
+        private byte[] m_ReceiveBuffer;
+
+        private int m_SendCount;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
+        private byte[] m_SendBuffer;
+
         /// <summary>
         /// Write
         /// </summary>
-        public uint Control;
+        public uint Control { get => m_Control; set => m_Control = value; }
 
         /// <summary>
         /// The rx count
         /// </summary>
-        public int ReceiveCount;
+        public int ReceiveCount { get => m_ReceiveCount; set => m_ReceiveCount = value; }
 
         /// <summary>
         /// Read only
         /// </summary>
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
-        public byte[] ReceiveBuffer;
+        public byte[] ReceiveBuffer { get => m_ReceiveBuffer; set => m_ReceiveBuffer = value; }
 
         /// <summary>
         /// Write
         /// </summary>
-        public int SendCount;
+        public int SendCount { get => m_SendCount; set => m_SendCount = value; }
 
         /// <summary>
         /// Write
         /// </summary>
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
-        public byte[] SendBuffer;
+        public byte[] SendBuffer { get => m_SendBuffer; set => m_SendBuffer = value; }
     }
 }
