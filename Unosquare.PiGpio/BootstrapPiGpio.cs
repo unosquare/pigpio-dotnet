cfe1be3a0936f164d704b6a8e86c6bb2861540fe
Geo Perez <geovanni.perez@gmail.com>
2018-12-19 14:31:03 -0600

    Add Bootstrap

---
+++ Unosquare.PiGpio/BootstrapPiGpio.cs
@@ -0,0 +1,23 @@
+﻿namespace Unosquare.PiGpio
+{
+    using RaspberryIO.Abstractions;
+
+    /// <summary>
+    /// Represents the bootstrap process to use PiGPio.
+    /// </summary>
+    /// <seealso cref="IBootstrap" />
+    public class BootstrapPiGpio : IBootstrap
+    {
+        private static readonly object SyncLock = new object();
+
+        /// <inheritdoc />
+        public void Bootstrap()
+        {
+            lock (SyncLock)
+            {
+                // TODO: Include extract resource
+                // Resources.EmbeddedResources.ExtractAll();
+            }
+        }
+    }
+}
